20b231cb811f7264b1700cb1c3a0ebaa4b28ebbb
alz-droid <root@DESKTOP-A7FK83I.localdomain>
2022-10-08 23:52:05 +1300

    added codes in .ipynb and change the codes in R file

--- assign_2.R
+++ assign_2.R
@@ -6,54 +6,74 @@ library(stringr)
 library(rvest)
 library(xml2)
 library(polite)
+library(lubridate)
 
-url <- "https://www.metacritic.com/game"
-games_page <- url %>% 
+# # Helper function
+# get_data <- function(data, element_class) {
+#   text_data <- data %>% 
+#     html_nodes(element_class) %>% 
+#     html_text2()
+#   
+#   return(text_data)
+# }
+
+# URL of the new games in all platforms in the last 90 days from metacritic.
+url <- "https://www.metacritic.com/browse/games/score/metascore/90day/all/filtered"
+
+# Read and print the content of metacritic's New Games page.
+new_games <- url %>% 
   read_html()
-games_page
+new_games
 
-games_page %>% 
+# Shows the html structure of the New Games page.
+new_games %>% 
   html_structure()
 
-# Helper function
-get_data <- function(data, element_class) {
-  text_data <- data %>% 
-    html_nodes(element_class) %>% 
-    html_text2()
-  
-  return(text_data)
-}
+# Finds all the h3 element under the .title class which is under .clamp-summary-wrap class.
+# Then extract the raw text inside the h3 element. Assign the result as titles.
+titles <- new_games %>% 
+  html_nodes(".clamp-summary-wrap > .title > h3") %>% 
+  html_text()
+titles
 
-# Gaming Platforms
-platforms <- tibble(Gaming_Platforms = get_data(games_page, ".platforms.current_platforms > li"))
-platforms %>% 
-  mutate(Ref = games_page %>% html_nodes(".platforms.current_platforms > li > div > span > a") %>% html_attr("href"))
-  
-# Legacy Platforms
-legacy <- tibble(Legacy = get_data(games_page, ".platforms.legacy_platforms > li"))
-legacy
+# Finds the element inside .platform class having .data class. Then extract the raw text inside that element. 
+# Assign the result as platform. 
+platform <- new_games %>% 
+  html_nodes(".platform > .data") %>% 
+  html_text()
+platform
 
-# New Game Releases
-new_games <- tibble(Title = get_data(games_page, "th.product_title"),
-                    Platform = get_data(games_page, ".product_platform"),
-                    Score = get_data(games_page, ".metascore_anchor > div"),
-                    Critics = get_data(games_page, ".critic_count > a > span"))
-new_games 
+# Finds all of the span element inside .clamp-details class and extract the contained raw text from it. 
+# Assign the result as release_date.
+release_date <- new_games %>% 
+  html_nodes(".clamp-details > span") %>% 
+  html_text()
+release_date
 
-# Upcoming Games
-coming_soon_games <- tibble(Title = get_data(games_page, ".product_title > a"))
-coming_soon_games
+# Takes the element with class .summary then extract the text inside of it. Save th result as game_descr.
+description <- new_games %>% 
+  html_nodes(".summary") %>% 
+  html_text()
+description
 
-# Top Games info
-top_games <- tibble(Title = get_data(games_page, ".title > h3"),
-                    Release = get_data(games_page, ".clamp-details > span"),
-                    Meta_Score = get_data(games_page, ".clamp-metascore > a > div.metascore_w.large.game.positive"),
-                    Description = get_data(games_page, ".summary"))
-top-games
-  
-# Games Genres
-genre <- tibble(Genre = get_data(games_page, "ul.genre_nav > li > a"))
-genre
+# Finds the div element inside a element under the element having .clamp-metascore as class and extract the text inside.
+# Save the result as meta_score.
+meta_score <- new_games %>% 
+  html_nodes(".clamp-metascore > a > div") %>% 
+  html_text()
+meta_score
 
+# Finds the div element inside a element under the element having .clamp-usercore as class and extract the text inside.
+# Save the result as user_score.
+user_score <- new_games %>% 
+  html_nodes(".clamp-userscore > a > div") %>% 
+  html_text()
+user_score
 
-
+new_games_df <- tibble(game_title = titles,
+                       release_date = as.Date(release_date, format = "%B %d, %Y"),
+                       platform = platform %>% str_replace_all("[^[:alnum:]]", " ") %>% trimws("both"),
+                       games_descr = description %>% str_replace_all("[^[:alnum:]]", " ") %>% trimws("both"),
+                       meta_score = meta_score,
+                       user_score = user_score)
+new_games_df %>% head()
