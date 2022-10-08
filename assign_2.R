2167d4a0e96553e8f36301af8f97e49fe2383c7e
Math User <mathuser@mathvlinux335p.canterbury.ac.nz>
2022-10-08 13:46:44 +1300

    add codes in jupyter notebook and change some of the codes in R file

--- assign_2.R
+++ assign_2.R
@@ -6,7 +6,6 @@ library(stringr)
 library(rvest)
 library(xml2)
 library(polite)
-library(fuzzyjoin)
 
 url <- "https://www.metacritic.com/game"
 games_page <- url %>% 
@@ -30,53 +29,31 @@ platforms <- tibble(Gaming_Platforms = get_data(games_page, ".platforms.current_
 platforms %>% 
   mutate(Ref = games_page %>% html_nodes(".platforms.current_platforms > li > div > span > a") %>% html_attr("href"))
   
+# Legacy Platforms
+legacy <- tibble(Legacy = get_data(games_page, ".platforms.legacy_platforms > li"))
+legacy
 
 # New Game Releases
 new_games <- tibble(Title = get_data(games_page, "th.product_title"),
-                    Platform = get_data(games_page, ".product_platform") %>% 
-                      str_extract(regex("(?<=(on ))[a-zA-Z0-9^]+")),
+                    Platform = get_data(games_page, ".product_platform"),
                     Score = get_data(games_page, ".metascore_anchor > div"),
                     Critics = get_data(games_page, ".critic_count > a > span"))
 new_games 
 
-# Top games info
-top_games <- games_page %>% 
-  html_nodes(".title > h3") %>% 
-  html_text2()
-top_games
+# Upcoming Games
+coming_soon_games <- tibble(Title = get_data(games_page, ".product_title > a"))
+coming_soon_games
 
-release_date <- games_page %>% 
-  html_nodes(".clamp-details > span") %>% 
-  html_text2()
-release_date
-
-meta_score <- games_page %>% 
-  html_nodes(".clamp-metascore > a > div.metascore_w.large.game.positive") %>% 
-  html_text2()
-meta_score
-
-game_description <- games_page %>% 
-  html_nodes(".summary") %>% 
-  html_text2()
-game_description
-
-datas <- tibble(Game = top_games, Release = release_date, Meta_Score = meta_score, Detail = game_description)
-datas
-
-# datas %>% 
-#   write_csv("temporary.csv")
+# Top Games info
+top_games <- tibble(Title = get_data(games_page, ".title > h3"),
+                    Release = get_data(games_page, ".clamp-details > span"),
+                    Meta_Score = get_data(games_page, ".clamp-metascore > a > div.metascore_w.large.game.positive"),
+                    Description = get_data(games_page, ".summary"))
+top-games
   
-genre <- games_page %>% 
-  html_nodes("ul.genre_nav > li > a") %>% 
-  html_text2()
-genre <- tibble(Genre = genre)
+# Games Genres
+genre <- tibble(Genre = get_data(games_page, "ul.genre_nav > li > a"))
+genre
 
-legacy <- games_page %>% 
-  html_nodes(".platforms.legacy_platforms > li") %>% 
-  html_text2()
-legacy <- tibble(Legacy = legacy)
 
-coming_soon_product <- games_page %>% 
-  html_nodes(".product_title > a") %>% 
-  html_text2()
-coming_soon_product <- tibble(coming_soon_product)
+
